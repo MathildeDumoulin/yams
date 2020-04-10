377525aaed6d64f5cf9523f43754c4a704c572e2
Eva <eva.benharira@gmail.com>
2020-04-10 15:34:39 +0200

    update main pour test Dice

--- src/yams_maths/Main.java
+++ src/yams_maths/Main.java
@@ -3,5 +3,9 @@ package yams_maths;
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("Hello world");
+		Dice de1 = new Dice();
+		System.out.println("init de1 = "+ de1.getValue());
+		de1.lancerDice();
+		System.out.println("lancer de1 = "+ de1.getValue());
 	}
 }
